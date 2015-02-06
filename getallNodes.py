c04869645eedf813f44fe19531dd0b2ba7c44116
candice Lin <candii.lin@gmail.com>
2015-02-05 19:59:47 -0800

    get nodes file

---
+++ getallNodes.py
@@ -0,0 +1,24 @@
+import xmlrpclib
+import operator
+
+api_server = \
+xmlrpclib.ServerProxy('https://www.planet-lab.org/PLCAPI/')
+
+auth={}
+auth['Username']="frhjing@interchange.ubc.ca"
+# fill with your password
+auth['AuthString']="#"
+auth['AuthMethod']="password"
+
+nodes = api_server.GetNodes(auth)
+
+len(nodes)
+
+hostnames = map(operator.itemgetter("hostname"), nodes)
+
+f = open('myfile.txt','w')
+
+for host in hostnames:
+	f.write(host + '\n')
+
+f.close()
