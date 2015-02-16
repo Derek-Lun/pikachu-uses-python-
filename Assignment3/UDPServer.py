f891e85aa3bb331ee54e23e525fdc7ad1bb1545b
Candice Lin <candii.lin@gmail.com>
2015-02-16 14:21:55 -0800

    parsing code for command for server

--- Assignment3/UDPServer.py
+++ Assignment3/UDPServer.py
@@ -8,6 +8,17 @@ import select
 
 data = {}
 
+
+def parseCommand (recv):
+  request = {}
+  request['command'] = recv[:1]
+  request['key'] = recv[1:33]
+
+  if request['command'] == struct.pack('<B', 1):
+    length = struct.unpack_from('<h', recv, 33)
+    begin = 35
+    request['value'] = recv[begin:being+length]
+
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -19,3 +30,8 @@ while True:
   print "Listening on %s" % server_address[1]
 
   data, address = sock.recvfrom(16384)
+
+  req = parseCommand(data)
+
+
+
