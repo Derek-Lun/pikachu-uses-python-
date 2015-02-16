bdeb80f7798a4fd214869f37d2bee3cebeb16f20
Candice Lin <candii.lin@gmail.com>
2015-02-16 15:06:46 -0800

    udp server base

--- Assignment3/UDPServer.py
+++ Assignment3/UDPServer.py
@@ -1,23 +1,58 @@
 import socket
-import sys
-import array
 import struct
-import random
-import time
-import select
+
 
 data = {}
 
+def put ():
+  print 'put'
+
+def get ():
+  print 'get'
+
+def remove ():
+  print 'remove'
+
+def shutdown ():
+  print 'shutdown'
+
+command = {
+  0 : put,
+  1 : get,
+  2 : remove,
+  3 : shutdown
+}
+
+response_status = {
+  'success': 0,
+  'dne': 1,
+  'oos': 2,
+  'sys_overload': 3,
+  'internal_failutre': 4,
+  'do_not_recognize': 5
+}
 
 def parseCommand (recv):
   request = {}
   request['command'] = recv[:1]
   request['key'] = recv[1:33]
 
-  if request['command'] == struct.pack('<B', 1):
+  if request['command'] == 'put':
     length = struct.unpack_from('<h', recv, 33)
     begin = 35
     request['value'] = recv[begin:being+length]
+
+  return request
+
+def createReply (status, value = None):
+  reply = bytearray()
+
+  reply.append(struct.pack('<b',response_status[status]))
+
+  if value:
+    reply.extend(value)
+
+  return reply  
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -33,5 +68,12 @@ while True:
 
   req = parseCommand(data)
 
+  func = command[req['command']]
+
+  if func:
+    func()
+  else:
+    reply = createReply('do_not_recognize');
+    sock.sentto(reply, address)
 
 
