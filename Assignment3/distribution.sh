05fe698b6352e8027cafad58e2d2b70bd0c36f1a
kelvinau <kelvinau4413@hotmail.com>
2015-03-08 23:43:26 -0700

    added nohup

--- Assignment3/distribution.sh
+++ Assignment3/distribution.sh
@@ -11,5 +11,5 @@ do
     scp -i ~/.ssh/id_rsa ${file} ubc_eece411_5@${node}:node.txt
     scp -i ~/.ssh/id_rsa dist/UDPServer/* ubc_eece411_5@${node}:Group10/.
     scp -i ~/.ssh/id_rsa make-run.sh ubc_eece411_5@${node}:.
-    ssh -n -i ~/.ssh/id_rsa -l ubc_eece411_5 ${node} "chmod +x ~/Group10/UDPServer; echo '*/5 * * * * sh make-run.sh' | sudo crontab; ~/Group10/UDPServer &"
+    ssh -n -i ~/.ssh/id_rsa -l ubc_eece411_5 ${node} "chmod +x ~/Group10/UDPServer; echo '*/5 * * * * sh make-run.sh' | sudo crontab; nohup Group10/UDPServer &"
 done
